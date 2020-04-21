180edb8fcecb42be72468a2bb7b05cc9861956a2
Tai-Rocha <taina013@gmail.com>
2020-04-21 18:09:06 -0300

    setup_sdmdata function of modleR to clean records

---
+++ R_scripts/cleaning_the_data.R
@@ -0,0 +1,46 @@
+#############################################################
+## Cleaning georreferenced data for Carpornis melanocephala
+## Author: Tainá Rocha
+## Date: 21 April 2020
+## Laste update
+#############################################################
+
+## Read and load georreferenced data
+
+sabiapimenta_records <- read.csv("./data/c_melanocephala.csv", sep = ",", dec = ".")
+
+## Read and load the .shp file to use as a buffer paramenter (M BAM area)
+
+sabiapimenta_masc <- readOGR("./data/shapemasc/alt_masc.shp")
+
+### Bioclimatic variables from worldclim List and stack rescpectively
+
+predictor_sabiapimenta <- list.files(path = './data/layers/present',
+                        pattern = 'tif',
+                        full.names = T)
+
+predictors <- stack(predictor_sabiapimenta)
+
+plot(predictors)
+
+
+sdmdata_sabiapimenta <- setup_sdmdata(species_name = sabiapimenta_records[1],
+                             occurrences = sabiapimenta_records,
+                             predictors = predictors,
+                             models_dir = results,
+                             partition_type = "crossvalidation",
+                             cv_partitions = 5,
+                             cv_n = 10,
+                             seed = 512,
+                             buffer_type = "user",
+                             buffer_shape = alt_masc
+                             png_sdmdata = TRUE,
+                             n_back = 1000,
+                             clean_dupl = TRUE,
+                             clean_uni = TRUE,
+                             clean_nas = TRUE,
+                             geo_filt = FALSE,
+                             geo_filt_dist = 10,
+                             select_variables = TRUE,
+                             sample_proportion = 0.5,
+                             cutoff = 0.7)
