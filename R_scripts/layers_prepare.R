ebfff61cbc87820e2474712c61c039519aff5b70
Tai-Rocha <taina013@gmail.com>
2020-04-21 22:27:40 -0300

    in progress

--- R_scripts/layers_prepare.R
+++ R_scripts/layers_prepare.R
@@ -27,9 +27,24 @@ plot(af_extention)
 
 writeRaster(af_extention, "./data/layers/cropped_layers/af_cropped_alt.tif")
 
-###### M bam Area
+###### M bam Area ## Verificar a limpeza dos dados q caem em NA
 
 bam_sabiapimenta <- af_extention <= 800.00
 plot(bam_sabiapimenta)
 
-writeRaster(bam_sabiapimenta, "./data/layers/cropped_layers/bam_sabiapimenta_area.tif")
+values(bam_sabiapimenta)[values(bam_sabiapimenta) == 0] = NA
+plot(bam_sabiapimenta)
+
+writeRaster(bam_sabiapimenta, "./data/layers/cropped_layers/BAMM_sabiapimenta.tif")
+
+
+####### Pseudoabsence # testar
+
+pseudoabsence_area <- (af_extention > 830.000) & (af_extention < 900.00)
+plot(pseudoabsence_area)
+
+values(pseudoabsence_area)[values(pseudoabsence_area) == 0] = NA
+plot(pseudoabsence_area)
+
+writeRaster(pseudoabsence_area, "./data/layers/cropped_layers/pseudoabsence_area.tif")
+
