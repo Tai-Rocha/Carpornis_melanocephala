11630753b649b91e8f3e9c58d551395ab5467f67
Tai-Rocha <taina013@gmail.com>
2020-05-13 01:19:43 -0300

    in progress

--- R_scripts/correlation_statistic_variables.R
+++ R_scripts/correlation_statistic_variables.R
@@ -12,16 +12,54 @@ library(rgdal)
 library(corrplot)
 library(corrgram)
 
-### Load, List, Stack 
-dados <- list.files(path = "/home/taina/Documentos/Worldclim/Worldclim_AF_version_2.1/", pattern = ".tif", full.names=TRUE)
+### Load, List, Stack Layers
+dados <- list.files(path = "/home/taina/Documentos/Worldclim/Worldclim_Present_AF_version_2.1_&_Altitude_AF_INPE/", pattern = ".tif", full.names=TRUE)
 biovars <- stack(dados)
+plot(biovars)
 
-# extrair os valores dos pontos nas camadas ambientais
-vals <- extract(wclim, pts)
-summary(vals)
+##### Pontos aleatórios 
+points <- spsample(as(biovars@extent, 'SpatialPolygons'), n=1000, type="random")    
+plot(biovars[[1]])
+plot(points,add=T)
 
-# calcular a os indices de correlacao e niveis de significancia (data.frame)
-corrgram(vals, bg='blue', cex=1, pch=21, fig=TRUE, main = "Pearson Correlation 1", lower.panel=panel.pts, upper.panel=panel.conf)
+##### Pontos Planilha Original
+
+pontos_raw <-read.csv("./data/c_melanocephala_nova.csv", sep = ",", dec = ".")
+
+# remover registros duplicados
+
+dups.all <- duplicated(pontos_raw[, c('lat', 'long')])
+pontos_raw_unique <- pontos_raw[!dups.all, ]
+
+# Inverse (Extract values from point require a file in format long lat respectively to use extract funtion)
+
+long_lat_raw_unique <- rev(pontos_raw_unique[nrow(pontos_raw_unique):1,])
+
+
+######################################## 
+
+#### Extrair Valores nos Pontos Aletórios
+values <- raster::extract(biovars, points@coords, method='simple', df=T)
+
+#### Extrair Valores nos Pontos Originais
+
+values_in_coord <- extract(biovars, long_lat_raw_unique, method='simple', df=T)
+
+######################################## 
+
+#### Aleatórios calcular a os indices de correlacao e niveis de significancia (data.frame)
+corrgram(values[,-1], bg='blue', cex=1, pch=21, fig=TRUE, main = "Pearson Correlation", lower.panel=panel.pts, upper.panel=panel.conf)
+
+#### Pontos Raw calcular a os indices de correlacao e niveis de significancia (data.frame)
+corrgram(values_in_coord[,-1], bg='blue', cex=1, pch=21, fig=TRUE, main = "Pearson Correlation", lower.panel=panel.pts, upper.panel=panel.conf)
+
+
+
+
+
+
+##### testar script amanhã a partir daqui !!!!!!!!!!!!!!!!!!!!
+
 
 # biovar <- dropLayer(wclim, c('bio1', 'bio2', 'bio3', 'bio4', 'bio5', 'bio6', 'bio7', 'bio8', 'bio9', 'bio10', 'bio11', 'bio12', 'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19'))
 # deletar a linha acima as variaves selecionadas, perceba que apenas as variaveis descartadas devem permanecer na lista
