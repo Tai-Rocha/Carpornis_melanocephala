1a9525702e717a04de5ba8c5b4381ee06c117707
Tai-Rocha <taina013@gmail.com>
2020-05-16 23:00:34 -0300

    final

---
+++ R_scripts/2_do_many_ENM_6vars.R
@@ -0,0 +1,34 @@
+
+########################################################################
+## Ecological Niche Model Analysis for Carpornis melanocephala
+## Author : Tainá Rocha
+## Date: 13 May 2020
+########################################################################
+
+### Library
+
+library(modleR)
+
+###
+
+models_algorithms <- do_many (
+                            species_name= unique(sp_records$sp),
+                             predictors= predictors,
+                             models_dir= "./results_final_6vars",
+                             bioclim = TRUE, 
+                             domain = TRUE,
+                             glm = TRUE,
+                             mahal = FALSE, 
+                             maxent = TRUE, 
+                             maxnet = FALSE, 
+                             rf = TRUE,
+                             svmk = FALSE, 
+                             svme = FALSE, 
+                             brt = TRUE,
+                             dismo_threshold= c("sensitivity"),
+                             proc_threshold= 5,
+                             equalize = TRUE,
+                             project_model = T,
+                             proj_data_folder= "./data/layers/current_6vars",
+                             png_partitions= FALSE)
+
