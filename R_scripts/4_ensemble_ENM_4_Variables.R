2e9c561f0fe426117ab3b2afa0657e396270ab13
Tai-Rocha <taina013@gmail.com>
2020-05-20 23:10:09 -0300

    final

---
+++ R_scripts/4_ensemble_ENM_4_Variables.R
@@ -0,0 +1,18 @@
+ensemble_sp <- ensemble_model(species_name= unique(sp_records$sp),
+                              occurrences = sp_records[,-1],lon = "long",
+                              lat = "lat",
+                              models_dir= "./results_final_4vars",
+                              final_dir = "final_models",
+                              ensemble_dir = "ensemble", 
+                              proj_dir = "present", 
+                              algorithms = NULL,
+                              which_ensemble = c("average"), which_final = c("raw_mean","bin_mean" ,"bin_consensus","raw_mean_th","raw_mean_cut"),
+                              performance_metric = "TSSmax", 
+                              dismo_threshold = c("sensitivity"),
+                              consensus_level = 0.5,
+                              png_ensemble = TRUE, 
+                              write_occs = TRUE,
+                              write_map = TRUE, 
+                              scale_models = TRUE,
+                              uncertainty = TRUE)
+
