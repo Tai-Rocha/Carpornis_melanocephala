b90442df5fb595a3484abdd2846bfa9d3d2e9218
Tai-Rocha <taina013@gmail.com>
2020-06-01 19:41:46 -0300

    final update

--- R_scripts/4_ensemble_ENM_4_Variables.R
+++ R_scripts/4_ensemble_ENM_4_Variables.R
@@ -1,18 +1,44 @@
+#Present
+
 ensemble_sp <- ensemble_model(species_name= unique(sp_records$sp),
                               occurrences = sp_records[,-1],lon = "long",
                               lat = "lat",
                               models_dir= "./results_final_4vars",
                               final_dir = "final_models",
                               ensemble_dir = "ensemble", 
-                              proj_dir = "present", 
                               algorithms = NULL,
-                              which_ensemble = c("average"), which_final = c("raw_mean","bin_mean" ,"bin_consensus","raw_mean_th","raw_mean_cut"),
-                              performance_metric = "TSSmax", 
+                              which_ensemble = c("weighted_average"), 
+                              which_final = c("raw_mean","bin_mean" ,"bin_consensus","raw_mean_th","raw_mean_cut"),
+                              performance_metric = c("TSSmax"), 
                               dismo_threshold = c("sensitivity"),
                               consensus_level = 0.5,
                               png_ensemble = TRUE, 
                               write_occs = TRUE,
                               write_map = TRUE, 
                               scale_models = TRUE,
-                              uncertainty = TRUE)
+                              uncertainty = TRUE,
+                              overwrite = T)
+
+
+## Present Projection
+
+ensemble_sp <- ensemble_model(species_name= unique(sp_records$sp),
+                              occurrences = sp_records[,-1],lon = "long",
+                              lat = "lat",
+                              models_dir= "./results_final_4vars",
+                              final_dir = "final_models",
+                              ensemble_dir = "ensemble", 
+                              proj_dir = "current_4vars" ,
+                              algorithms = NULL,
+                              which_ensemble = c("weighted_average"), 
+                              which_final = c("raw_mean","bin_mean" ,"bin_consensus","raw_mean_th","raw_mean_cut"),
+                              performance_metric = c("TSSmax"), 
+                              dismo_threshold = c("sensitivity"),
+                              consensus_level = 0.5,
+                              png_ensemble = TRUE, 
+                              write_occs = TRUE,
+                              write_map = TRUE, 
+                              scale_models = TRUE,
+                              uncertainty = TRUE,
+                              overwrite = T)
 
