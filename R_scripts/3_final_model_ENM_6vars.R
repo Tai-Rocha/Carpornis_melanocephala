ef879a721bd9bc509172164f08740301271b75f5
Tai-Rocha <taina013@gmail.com>
2020-06-01 19:27:46 -0300

    final update

--- R_scripts/3_final_model_ENM_6vars.R
+++ R_scripts/3_final_model_ENM_6vars.R
@@ -1,4 +1,4 @@
-
+## Present 
 finalModels_sp <-final_model(species_name = unique(sp_records$sp),
                  algorithms = NULL, #if null it will take all the algorithms in disk
                  scale_models = FALSE,
@@ -10,5 +10,20 @@ finalModels_sp <-final_model(species_name = unique(sp_records$sp),
                  consensus_level = 0.5,
                  png_final= TRUE,
                  uncertainty = TRUE,
-                 proj_dir = "current_6vars",
                  overwrite = TRUE)
+
+
+## Present Projection
+finalModels_sp <-final_model(species_name = unique(sp_records$sp),
+                             algorithms = NULL, #if null it will take all the algorithms in disk
+                             scale_models = FALSE,
+                             models_dir = "./results_final_6vars",
+                             sensitivity = 0.9,
+                             which_models = c("raw_mean","bin_mean","bin_consensus","raw_mean_th","raw_mean_cut"),
+                             mean_th_par = c("sensitivity"),
+                             final_dir = "final_models",
+                             consensus_level = 0.5,
+                             png_final= TRUE,
+                             uncertainty = TRUE,
+                             proj_dir = "current_6vars",
+                             overwrite = TRUE)
