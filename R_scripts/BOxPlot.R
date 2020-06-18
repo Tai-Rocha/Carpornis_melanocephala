8ac0ba1da394a4dc47d674e4c01527b45210065e
Tai-Rocha <taina013@gmail.com>
2020-06-18 15:19:10 -0300

    Boxplot of Values

---
+++ R_scripts/BOxPlot.R
@@ -0,0 +1,44 @@
+#ativar o pacote ggplot2. Pode ser pelo comando library(ggplot2) ou manualmente no
+library(ggplot2)
+
+#canto direiro inferior, aba "Packages".
+
+
+
+#No script abaixo, "MD_AUC"  o nome que dei pra planilha ao abrir aqui,
+
+#Footprint = nome da coluna em quest?o
+# Comando Para apargar arquivos: 
+rm(KAPPA) 
+
+
+#(1) Basic boxplot
+
+AUC <- read.csv(file.choose(), header=T)
+
+AUC <- ggplot(AUC_Table, aes(x = Algorithm, y = AUC)) +
+  
+  geom_boxplot()
+
+AUC
+
+AUC <- AUC + scale_y_continuous(name = "AUC", breaks=seq(0,1), limits=c(0,1))
+
+AUC
+
+###########################################################
+
+#(1) Basic boxplot
+
+TSS <- read.csv(file.choose(), header=T)
+
+TSS <- ggplot(TSS, aes(x = Algorithms, y = TSS)) +
+  
+  geom_boxplot()
+
+TSS
+
+TSS <- TSS + scale_y_continuous(name = "TSS", breaks=seq(0,1), limits=c(0,1))
+
+TSS
+
